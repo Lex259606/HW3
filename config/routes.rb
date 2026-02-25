fde0ce4d92e9135b82ec0e63993e1ef8d18a31c3
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-02-25 21:35:41 +0000

    Finished Travel Log Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,33 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "places", :action => "index" })
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  # Places
+  get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  get("/places/:id", { :controller => "places", :action => "show" })
+  post("/places", { :controller => "places", :action => "create" })
+
+  # Entries
+  get("/entries/new", { :controller => "entries", :action => "new" })
+  post("/entries", { :controller => "entries", :action => "create" })
+end
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"] })
+  end
+
+  def new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
 end
